e38d410c80ae0d8b4541f238d26d9dcf8a6ce2a2
Markus Heer <mh@markusheer.ch>
2013-05-31 14:51:25 +0200

    Add keyboard shortcuts to extension

--- js/popup.js
+++ js/popup.js
@@ -3,13 +3,18 @@
  */
 Pathmarks.PopUp = Class.extend({
 
+
     init: function() {
+        this.KEY_UP = 38;
+        this.KEY_DOWN = 40;
+        this.KEY_ENTER = 13;
         this.core = new Pathmarks.Core();
     },
 
     start: function() {
         this.showBaseUrl();
         this.loadConfiguration();
+        this.loadKeyNavigation();
     },
 
     getHostAndPort: function(url) {
@@ -39,7 +44,7 @@ Pathmarks.PopUp = Class.extend({
                     url.addClass("url");
                     url.attr("data-path", entry.value);
                     url.html(entry.title + "<span class='path'>" + entry.value + "</span>");
-                    url.click(function () {
+                    url.on("click", function () {
                         self.changeUrls(jQuery(this).attr("data-path"));
                     });
                     urls.append(url);
@@ -55,6 +60,48 @@ Pathmarks.PopUp = Class.extend({
         chrome.tabs.getSelected(null, function(tab) {
             var targetForTab = self.getHostAndPort(tab.url);
             jQuery(".baseurl").html(targetForTab);
+        });
+    },
+
+    loadKeyNavigation: function() {
+        var self = this;
+        jQuery(document.documentElement).on("keyup", function(event) {
+            var selectedUrl = null;
+            if (jQuery(".url").length == 0) {
+                return;
+            }
+            if (event.which == self.KEY_ENTER) {
+                jQuery(".selected").click();
+            }
+            if (event.which == self.KEY_UP || event.which == self.KEY_DOWN) {
+                selectedUrl = jQuery(".selected");
+                if (selectedUrl.length == 0) {
+                    jQuery(".url:first").addClass("selected");
+                    return;
+                }
+            }
+            if (event.which == self.KEY_DOWN) {
+                selectedUrl = jQuery(".selected");
+                var nextUrl = selectedUrl.next(".url");
+                selectedUrl.removeClass("selected");
+                if (nextUrl.length == 0) {
+                    jQuery(".url:first").addClass("selected");
+                    return;
+                }
+                nextUrl.addClass("selected");
+                return;
+            }
+            if (event.which == self.KEY_UP) {
+                selectedUrl = jQuery(".selected");
+                var previousUrl = selectedUrl.prev(".url");
+                selectedUrl.removeClass("selected");
+                if (previousUrl.length == 0) {
+                    jQuery(".url:last").addClass("selected");
+                    return;
+                }
+                previousUrl.addClass("selected");
+                return;
+            }
         });
     }
 
