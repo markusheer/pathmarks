4aefc22d414119afec1c2b7ca851a040b7915112
Markus Heer <mh@markusheer.ch>
2013-05-31 16:30:23 +0200

    Add a dev icon for easier chrome testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,8 +32,28 @@ module.exports = function(grunt) {
         clean: ["target"]
     });
 
+    grunt.registerTask('changeToProdIcon', 'Change the icon in the manifest file', function() {
+        var manifest = grunt.file.readJSON('manifest.json');
+        manifest.browser_action.default_icon['19'] = 'images/icon19.png';
+        manifest.browser_action.default_icon['38'] = 'images/icon38.png';
+        manifest.icons['16'] = 'images/icon16.png';
+        manifest.icons['48'] = 'images/icon48.png';
+        manifest.icons['128'] = 'images/icon128.png';
+        grunt.file.write('manifest.json', JSON.stringify(manifest, null, '  ') + '\n');
+    });
+
+    grunt.registerTask('changeToDevIcon', 'Change the icon in the manifest file', function() {
+        var manifest = grunt.file.readJSON('manifest.json');
+        manifest.browser_action.default_icon['19'] = 'images/icon-dev19.png';
+        manifest.browser_action.default_icon['38'] = 'images/icon-dev38.png';
+        manifest.icons['16'] = 'images/icon-dev16.png';
+        manifest.icons['48'] = 'images/icon-dev48.png';
+        manifest.icons['128'] = 'images/icon-dev128.png';
+        grunt.file.write('manifest.json', JSON.stringify(manifest, null, '  ') + '\n');
+    });
+
     grunt.loadNpmTasks('grunt-zip');
     grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask('default', 'zip');
+    grunt.registerTask('default', ['changeToProdIcon', 'zip', 'changeToDevIcon']);
 };
--- images/convert.md
+++ images/convert.md
@@ -3,3 +3,7 @@
 Use the following shell command with graphicsmagick to create a new icon set:
 
 `for i in 128 48 38 19 16; do gm convert -resize ${i}x${i} iconmonstr-note-27-icon.png icon${i}.png; done`
+
+Use the following shell command to colorize an icon:
+
+`gm convert iconmonstr-note-27-icon.png -fill red -colorize 40/40/10 icon-dev.png`
