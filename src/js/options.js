640176d7f7f09ae953ad3ca19121d848d92a8fdf
Markus Heer <mh@markusheer.ch>
2018-08-29 11:26:30 +0200

    Refactor to remove code duplications

--- src/js/options.js
+++ src/js/options.js
@@ -1,4 +1,3 @@
-
 import * as Key from './key.js';
 import Core from './core.js';
 
@@ -26,104 +25,105 @@ class PathmarksOptions {
 	}
 
 	static setMessage(msg) {
-		const messageContainer = document.querySelector('.message');
-		messageContainer.classList.remove('error');
-		messageContainer.innerHTML = msg;
+		PathmarksOptions.changeMessage(msg);
 	}
 
 	static setErrorMessage(errorMsg) {
+		PathmarksOptions.changeMessage(errorMsg, true);
+	}
+
+	static changeMessage(msg, isError = false) {
 		const messageContainer = document.querySelector('.message');
-		messageContainer.classList.add('error');
-		messageContainer.innerHTML = errorMsg;
+		PathmarksOptions.changeClassState(messageContainer, 'error', isError);
+		messageContainer.innerHTML = msg;
 	}
 
 	static validateConfiguration() {
-		const configArea = document.querySelector('.configarea');
-		const jsonConfig = configArea.value;
-		if (!jsonConfig) {
+		const configArea = new ConfigArea();
+		if (!configArea.getValue()) {
 			PathmarksOptions.setMessage('Please click on save to clear the configuration.');
+			configArea.setValidState();
 			return true;
 		}
 		try {
-			JSON.parse(jsonConfig);
+			JSON.parse(configArea.getValue());
 			PathmarksOptions.setMessage('Please click on save to save the changed configuration.');
+			configArea.setValidState();
 			return true;
 		} catch (e) {
-			configArea.classList.add('invalid');
+			configArea.setInvalidState();
 			PathmarksOptions.setErrorMessage(`Error: JSON configuration is not valid ${e}`);
 			return false;
 		}
 	}
 
 	saveConfiguration() {
-		const configArea = document.querySelector('.configarea');
-		const jsonConfig = configArea.value;
-		if (!jsonConfig) {
+		const configArea = new ConfigArea();
+		if (!configArea.getValue()) {
 			this.resetConfiguration();
 			return;
 		}
 		try {
-			JSON.parse(jsonConfig);
+			JSON.parse(configArea.getValue());
 		} catch (e) {
-			configArea.classList.add('invalid');
+			configArea.setInvalidState();
 			PathmarksOptions.setErrorMessage(`Error: Can not save illegal JSON configuration ${e}`);
 			return;
 		}
-		this.core.useSetStorage(jsonConfig, () => {
-			configArea.classList.remove('invalid');
-			configArea.classList.add("saved");
+		this.core.useSetStorage(configArea.getValue(), () => {
+			configArea.setSavedState();
 			PathmarksOptions.setMessage('Configuration saved');
 		});
 	}
 
 	resetConfiguration() {
-		const configArea = document.querySelector('.configarea');
-		configArea.value = '';
-		configArea.classList.remove('invalid');
-		configArea.classList.add("saved");
+		const configArea = new ConfigArea();
+		configArea.reset();
 		this.core.useSetStorage('', () => {
 			PathmarksOptions.setMessage('Configuration cleared.');
 		});
 	}
 
 	loadConfiguration() {
-		this.core.useGetStorage(function loadItemsToTextarea(items) {
+		this.core.useGetStorage(items => {
 			if (items) {
-				document.querySelector('.configarea').value = items;
+				const configArea = new ConfigArea();
+				configArea.setValue(items);
 			}
 		});
 	}
 
 	addEntryFromInputFields() {
-		const titleField = document.querySelector('input[name=title]');
-		const valueField = document.querySelector('input[name=value]');
-		const title = titleField.value;
-		const value = valueField.value;
-		if (!title) {
-			titleField.classList.add('invalid');
-		} else {
-			titleField.classList.remove('invalid');
-		}
-		if (!value) {
-			valueField.classList.add('invalid');
-		} else {
-			valueField.classList.remove('invalid');
-		}
-		if (!title || !value) {
+		const titleField = new InputField('title');
+		const valueField = new InputField('value');
+		titleField.changeClassState();
+		valueField.changeClassState();
+		if (!titleField.getValue() || !valueField.getValue()) {
 			return;
 		}
-		this.core.useGetStorage((items) => {
+		this.core.useGetStorage(items => {
 			let configValues = [];
 			if (items) {
 				configValues = JSON.parse(items);
 			}
-			const newEntry = {title: title, value: value};
+			const newEntry = {
+				title: titleField.getValue(),
+				value: valueField.getValue()
+			};
 			configValues.push(newEntry);
 			document.querySelector('.configarea').value = Core.serializeConfigValues(configValues);
 			this.saveConfiguration();
-			titleField.value = '';
-			valueField.value = '';
+			titleField.resetValue();
+			valueField.resetValue();
 		});
+	}
+
+	static changeClassState(elem, className, condition) {
+		if (condition) {
+			elem.classList.add(className);
+		} else {
+			elem.classList.remove(className);
+		}
 	}
 
 	static addEntriesOnEnter(event, optionsObject) {
@@ -143,6 +143,64 @@ class PathmarksOptions {
 		document.querySelector('.header-version')
 			.innerHTML = `${manifest.version}${dev}`;
 	}
+}
+
+class ConfigArea {
+
+	constructor() {
+		this.configArea = document.querySelector('.configarea');
+	}
+
+	getValue() {
+		return this.configArea.value;
+	}
+
+	setValue(value) {
+		this.configArea.value = value;
+	}
+
+	reset() {
+		this.setValue('');
+		this.setSavedState();
+	}
+
+	setValidState() {
+		this.configArea.classList.remove('invalid')
+	}
+
+	setInvalidState() {
+		this.configArea.classList.add('invalid')
+	}
+
+	setSavedState() {
+		this.setValidState();
+		this.configArea.classList.add('saved');
+	}
+
+}
+
+class InputField {
+
+	constructor(fieldName) {
+		this.inputField = document.querySelector(`input[name=${fieldName}]`);
+	}
+
+	getValue() {
+		return this.inputField.value;
+	}
+
+	setValue(value) {
+		this.inputField.value = value;
+	}
+
+	resetValue() {
+		this.setValue('');
+	}
+
+	changeClassState() {
+		PathmarksOptions.changeClassState(this.inputField, 'invalid', !this.getValue());
+	}
+
 }
 
 document.addEventListener("DOMContentLoaded", () => {
